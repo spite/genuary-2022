c5e8a309d8829102c0af08c11199d23cddb5b318
thespite <the.spite@gmail.com>
2022-01-09 21:43:16 +0000

    added 5

---
+++ 5/main.js
@@ -0,0 +1,277 @@
+import {
+  scene,
+  getControls,
+  renderer,
+  addUpdate,
+  camera,
+  addResize,
+  resize,
+} from "../modules/renderer.js";
+import {
+  BufferGeometry,
+  Line,
+  LineBasicMaterial,
+  Points,
+  BufferAttribute,
+  PointsMaterial,
+  Vector2,
+  Vector3,
+} from "../third_party/three.module.js";
+import { perlin2 } from "../third_party/perlin.js";
+import { randomInRange, mod } from "../modules/Maf.js";
+import { DOFRenderer } from "../modules/dof-renderer.js";
+
+import * as dat from "../third_party/dat.gui.module.js";
+
+class Params {
+  constructor() {
+    this.exposure = 1;
+    this.iso = 1;
+    this.focalDistance = 50;
+    this.dofStrength = 2;
+    this.dofRange = 0.01;
+  }
+}
+const params = new Params();
+
+// const gui = new dat.GUI();
+// gui.add(params, "exposure", 0.01, 1).listen();
+// gui.add(params, "iso", 1, 100).listen();
+// gui.add(params, "focalDistance", 0, 100).listen();
+// gui.add(params, "dofStrength", 0.01, 3).listen();
+// gui.add(params, "dofRange", 0, 1).listen();
+
+function syncParams() {
+  dofRenderer.setParams({
+    focalDistance: params.focalDistance,
+    dofRange: params.dofRange,
+    dofStrength: params.dofStrength,
+    iso: params.iso,
+    exposure: params.exposure,
+  });
+}
+
+// for (const controller of gui.__controllers) {
+//   controller.onChange((e) => {
+//     dofRenderer.reset();
+//     syncParams();
+//   });
+// }
+
+const dofRenderer = new DOFRenderer(renderer);
+
+const controls = getControls();
+
+const POINTS = 300;
+
+function generateSquare() {
+  const points = [];
+  const r = 1;
+  const s = Math.sqrt(2) / 2;
+  for (let i = 0; i < POINTS; i++) {
+    const a = (i * 2 * Math.PI) / POINTS;
+    let x = r * Math.cos(a);
+    let y = r * Math.sin(a);
+    if (x < -s) x = -s;
+    if (x > s) x = s;
+    if (y < -s) y = -s;
+    if (y > s) y = s;
+    points.push(new Vector2(x, y));
+  }
+  return points;
+}
+
+function generateCircle() {
+  const points = [];
+  const r = 1;
+  for (let i = 0; i < POINTS; i++) {
+    const a = (i * 2 * Math.PI) / POINTS;
+    const x = r * Math.cos(a);
+    const y = r * Math.sin(a);
+    points.push(new Vector2(x, y));
+  }
+  return points;
+}
+
+function generatePolygon(sides) {
+  const points = [];
+  const b = (2 * Math.PI) / sides;
+  const r = 1;
+  const p0 = new Vector2();
+  const p1 = new Vector2();
+  for (let i = 0; i < POINTS; i++) {
+    const a = (i * 2 * Math.PI) / POINTS;
+    const g = mod(a, b);
+    const aa = (Math.floor((i * sides) / POINTS) * 2 * Math.PI) / sides;
+    p0.set(r * Math.cos(aa), r * Math.sin(aa));
+    p1.set(r * Math.cos(aa + b), r * Math.sin(aa + b));
+    const p = new Vector2();
+    p.lerpVectors(p0, p1, g / b);
+    points.push(p);
+  }
+  return points;
+}
+
+function generateBlob() {
+  const points = [];
+  const s = randomInRange(1, 5);
+  for (let i = 0; i < POINTS; i++) {
+    const a = (i * 2 * Math.PI) / POINTS;
+    const x = Math.cos(a);
+    const y = Math.sin(a);
+    const r = 1 + 0.5 + 0.5 * perlin2(x * s, y * s);
+    const p = new Vector2(x, y);
+    p.multiplyScalar(r);
+    points.push(p);
+  }
+  return points;
+}
+
+const sq = generateSquare();
+
+const material = new LineBasicMaterial({ color: 0xff00ff });
+// const material = new PointsMaterial({ color: 0xff00ff, size: 0.01 });
+function generateShape(shape, scale = 1) {
+  const points = [];
+  for (const p of shape) {
+    points.push(new Vector3(p.x, p.y, 0));
+  }
+  points.push(new Vector3(shape[0].x, shape[0].y, 0));
+  for (const p of points) {
+    p.multiplyScalar(scale);
+  }
+  const geometry = new BufferGeometry().setFromPoints(points);
+  const line = new Line(geometry, material);
+  return line;
+  const pts = new Points(geometry, material);
+  return pts;
+}
+
+controls.addEventListener("change", (e) => dofRenderer.reset());
+
+const axis = new Vector3(0, 0, 1);
+
+function blendShape(s0, s1) {
+  while (dofRenderer.scene.children.length) {
+    const l = dofRenderer.scene.children[0];
+    dofRenderer.scene.remove(l);
+    l.geometry.dispose();
+  }
+
+  const fRot = randomInRange(0.05, 0.05);
+  const fScale = randomInRange(9, 12);
+
+  const STEPS = 50;
+  const p = new Vector2();
+  const shapes = [];
+  for (let i = 0; i < STEPS; i++) {
+    const shape = [];
+    const a = (fRot * (i * 2 * Math.PI)) / STEPS;
+    for (let j = 0; j < s0.length; j++) {
+      const z = 1 - i / STEPS;
+      const t = i / STEPS;
+      p.copy(s0[j]).lerp(s1[j], t);
+      const pt = new Vector3(p.x, p.y, z);
+      pt.applyAxisAngle(axis, a);
+      pt.multiplyScalar(fScale);
+      shape.push(pt);
+    }
+    shapes.push(shape);
+  }
+
+  const LINES = POINTS * STEPS;
+  const lineGeometry = new BufferGeometry();
+  const fromPositions = new Float32Array(LINES * 3);
+  const toPositions = new Float32Array(LINES * 3);
+  const lineSizes = new Float32Array(LINES);
+  const lineSeeds = new Float32Array(LINES);
+  const lineColors = new Float32Array(LINES * 3);
+
+  let ptr = 0;
+  for (let i = 0; i < STEPS; i++) {
+    const shape = shapes[i];
+    for (let j = 0; j < POINTS; j++) {
+      const from = shape[j];
+      const to = shape[mod(j + 1, POINTS)];
+      fromPositions[ptr * 3] = from.x;
+      fromPositions[ptr * 3 + 1] = from.y;
+      fromPositions[ptr * 3 + 2] = from.z;
+      toPositions[ptr * 3] = to.x;
+      toPositions[ptr * 3 + 1] = to.y;
+      toPositions[ptr * 3 + 2] = to.z;
+      lineSizes[ptr] = 1;
+      lineSeeds[ptr] = Math.random() * 1000000;
+      // const { r, g, b } = gradient.getAt(ptr / LINES);
+      const [r, g, b] = [1, 1, 1];
+      lineColors[ptr * 3] = r;
+      lineColors[ptr * 3 + 1] = g;
+      lineColors[ptr * 3 + 2] = b;
+      ptr++;
+    }
+  }
+
+  lineGeometry.setAttribute("position", new BufferAttribute(fromPositions, 3));
+  lineGeometry.setAttribute("to", new BufferAttribute(toPositions, 3));
+  lineGeometry.setAttribute("size", new BufferAttribute(lineSizes, 1));
+  lineGeometry.setAttribute("seed", new BufferAttribute(lineSeeds, 1));
+  lineGeometry.setAttribute("color", new BufferAttribute(lineColors, 3));
+  dofRenderer.addLines(lineGeometry);
+}
+
+function randomize() {
+  dofRenderer.reset();
+  dofRenderer.setParams({
+    dofStrength: randomInRange(1.6, 2.2),
+    //    dofRange: randomInRange(0.01, 0.03),
+  });
+
+  const s = Math.floor(Math.random() * 3);
+  let shape;
+  switch (s) {
+    case 0:
+      shape = generateCircle();
+      break;
+    case 1:
+      shape = generateBlob();
+      break;
+    case 2:
+      const sides = Math.round(randomInRange(5, 8));
+      shape = generatePolygon(sides);
+  }
+
+  blendShape(sq, shape);
+}
+
+randomize();
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "KeyR") {
+    randomize();
+  }
+});
+
+document.querySelector("#randomizeBtn").addEventListener("click", (e) => {
+  randomize();
+});
+
+renderer.autoClear = false;
+
+camera.position.normalize().multiplyScalar(55);
+
+function render() {
+  dofRenderer.setParams({
+    focalDistance: 1 * camera.position.length(),
+  });
+
+  dofRenderer.render(camera);
+  renderer.setAnimationLoop(render);
+}
+
+function myResize(w, h) {
+  dofRenderer.setSize(w, h);
+}
+addResize(myResize);
+
+syncParams();
+resize();
+render();
---
+++ modules/dof-renderer.js
@@ -0,0 +1,342 @@
+import {
+  Scene,
+  Points,
+  RawShaderMaterial,
+  RGBAFormat,
+  AdditiveBlending,
+  FloatType,
+} from "../third_party/three.module.js";
+import { ShaderPass } from "./ShaderPass.js";
+import { getFBO } from "./fbo.js";
+
+const scene = new Scene();
+
+const calcBokeh = `
+float random(vec2 co, float time) {
+  return fract(sin(dot(co, vec2(12.9898, 78.233)) * time) * 43758.5453) * 2.0 - 1.0;
+}
+
+#define PI 3.14159265359
+
+vec3 randomSpherePoint(vec3 rand) {
+  float ang1 = (rand.x + 1.0) * PI; // [-1..1) -> [0..2*PI)
+  float u = rand.y; // [-1..1), cos and acos(2v-1) cancel each other out, so we arrive at [-1..1)
+  float u2 = u * u;
+  float sqrt1MinusU2 = sqrt(1.0 - u2);
+  float x = sqrt1MinusU2 * cos(ang1);
+  float y = sqrt1MinusU2 * sin(ang1);
+  float z = u;
+  return vec3(x, y, z);
+}
+
+vec2 randomCirclePoint(vec2 rand) {
+  float r = sqrt(rand.x);
+  float theta = rand.y * 2. * PI;
+  float x = r * cos(theta);
+  float y = r * sin(theta); 
+  return vec2(x, y);
+}
+
+// https://www.shadertoy.com/view/4djSRW
+
+vec4 hash44(vec4 p4) {
+  p4 = fract(p4  * vec4(.1031, .1030, .0973, .1099));
+    p4 += dot(p4, p4.wzxy+33.33);
+    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+}
+
+vec3 calcDir(vec3 position) {
+  vec4 rand = hash44(vec4(100.*position.xyz+vec3(seed),100.*time));
+  //vec3 dir = normalize(randomSpherePoint(rand));
+  vec2 dir = randomCirclePoint(rand.xy);
+  float dist = -position.z;
+  float d = abs(focalDistance-dist);
+  float rd = random(position.xz,time);
+  float r = (size / dist + dofRange * pow(d, dofStrength));
+  r = max(size / dist,r);
+  return vec3(dir.xy, r);  
+}
+`;
+
+const particleVS = `
+precision mediump float;
+
+attribute vec3 position;
+attribute float size;
+attribute vec3 color;
+
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+uniform float time;
+uniform float seed;
+uniform vec3 cameraPos;
+
+uniform float focalDistance;
+uniform float dofStrength;
+uniform float dofRange;
+uniform float iso;
+
+varying float vOpacity;
+varying vec3 vColor;
+
+${calcBokeh}
+
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  vec3 res = calcDir(mvPosition.xyz);
+  vec2 dir = res.xy;
+  float r = res.z;
+
+  vOpacity = (2.-pow(length(dir.xy),r));
+  vColor = color;
+  vColor *= iso;
+  vColor *= vOpacity;
+  //vColor = 1.-vec3((mvPosition.w-.1)/(1000.-.1)) ;
+  vColor = clamp(vec3(0.), vColor, vColor);
+
+  mvPosition.xy += dir.xy * r;
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = 1.;// + r;
+ 
+}
+`;
+
+const linesVS = `
+precision mediump float;
+
+attribute vec3 position;
+attribute vec3 to;
+attribute float size;
+attribute vec3 color;
+
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+uniform float time;
+uniform float seed;
+uniform vec3 cameraPos;
+uniform float iso;
+
+uniform float focalDistance;
+uniform float dofStrength;
+uniform float dofRange;
+
+varying float vOpacity;
+varying vec3 vColor;
+
+${calcBokeh}
+
+void main() {
+  float rr = .5 + .5 * random(position.xy + seed, time);
+  vec3 p = mix(position, to, rr);
+  vec4 mvPosition = modelViewMatrix * vec4(p, 1.0);
+  vec3 res = calcDir(mvPosition.xyz);
+  vec2 dir = res.xy;
+  float r = res.z;
+
+  vOpacity = (2.-pow(length(dir.xy),r));
+  vColor = color;
+  vColor *= iso;
+  vColor *= vOpacity;
+  //vColor = 1.-vec3((mvPosition.w-.1)/(1000.-.1)) ;
+  vColor = clamp(vec3(0.), vColor, vColor);
+
+  mvPosition.xy += dir.xy * r ;
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = 1.;// + r;
+  
+
+}`;
+
+const particleFS = `
+precision mediump float;
+
+varying float vOpacity;
+varying vec3 vColor;
+
+void main() {
+  gl_FragColor = vec4(vColor,1.);
+  gl_FragColor= vec4(1.);
+}
+`;
+
+const finalVS = `
+precision highp float;
+
+attribute vec3 position;
+attribute vec2 uv;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+varying vec2 vUv;
+
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1. );
+}`;
+
+const finalFS = `
+precision highp float;
+
+uniform sampler2D colorTexture;
+uniform float samples;
+uniform float exposure;
+
+varying vec2 vUv;
+
+// https://www.shadertoy.com/view/ttyXzt
+
+void main() {
+  vec4 c = texture2D(colorTexture, vUv);
+  /*float gamma = 2.2;
+  vec3 hdrColor = exposure * c.rgb / c.a;
+  vec3 mapped = hdrColor / (hdrColor + vec3(1.0));
+  mapped = pow(mapped, vec3(1.0 / gamma));*/
+
+  // vignette to darken the corners
+  vec2 uv = vUv - .5;
+  vec3 color =c.rgb / samples;//c.a;
+  //color *= c.a/samples;
+  
+	//color *= 1.-dot(uv,uv)*.8;
+
+  // exposure and tonemap
+  color *= exposure;
+  color = 1.-exp(color*-2.);
+
+  // gamma correction
+  color = pow(color, vec3(.45));
+
+  // make it pop
+  color = smoothstep(.02,.98,color);
+  
+  gl_FragColor = vec4(1.-color, 1.);
+ // gl_fragColor = vec4(c.a);
+}
+  `;
+
+function resize(w, h) {}
+
+class DOFRenderer {
+  constructor(renderer) {
+    this.acumSamples = 0;
+    this.maxSamples = 1024;
+    this.samples = 5;
+    this.renderer = renderer;
+    this.renderer.setClearColor(0, 0);
+    this.scene = new Scene();
+    this.colorFBO = getFBO(1, 1, { format: RGBAFormat, type: FloatType });
+    this.finalShader = new RawShaderMaterial({
+      uniforms: {
+        colorTexture: { value: this.colorFBO.texture },
+        samples: { value: 0 },
+        exposure: { value: 0 },
+      },
+      vertexShader: finalVS,
+      fragmentShader: finalFS,
+    });
+    this.finalPass = new ShaderPass(this.finalShader, 1, 1);
+    this.material = new RawShaderMaterial({
+      uniforms: {
+        time: { value: 0 },
+        focalDistance: { value: 100 },
+        dofStrength: { value: 1.5 },
+        dofRange: { value: 0.01 },
+        iso: { value: 1 },
+      },
+      depthTest: false,
+      depthWrite: false,
+      blending: AdditiveBlending,
+      vertexShader: particleVS,
+      fragmentShader: particleFS,
+      transparent: true,
+    });
+
+    this.lineMaterial = new RawShaderMaterial({
+      uniforms: {
+        time: { value: 0 },
+        focalDistance: { value: 100 },
+        dofStrength: { value: 1.5 },
+        dofRange: { value: 0.01 },
+        iso: { value: 1 },
+      },
+      depthTest: false,
+      depthWrite: false,
+      blending: AdditiveBlending,
+      vertexShader: linesVS,
+      fragmentShader: particleFS,
+      transparent: true,
+    });
+    this.setParams({
+      exposure: 1,
+      iso: 0.1,
+      focalDistance: 9.6,
+      dofRange: 0.01,
+      dofStrength: 1.5,
+    });
+  }
+
+  addPoints(geometry) {
+    const pointMesh = new Points(geometry, this.material);
+    this.scene.add(pointMesh);
+    return pointMesh;
+  }
+
+  addLines(geometry) {
+    const lineMesh = new Points(geometry, this.lineMaterial);
+    this.scene.add(lineMesh);
+    return lineMesh;
+  }
+
+  render(camera) {
+    if (this.acumSamples > this.maxSamples) {
+      return;
+    }
+    for (let j = 0; j < this.samples; j++) {
+      this.material.uniforms.time.value = Math.random(); //0.000001 * performance.now();
+      this.lineMaterial.uniforms.time.value = Math.random(); //0.000001 * performance.now();
+      this.finalPass.shader.uniforms.samples.value = this.acumSamples;
+      this.renderer.setRenderTarget(this.colorFBO);
+      //this.renderer.clearDepth();
+      this.renderer.render(this.scene, camera);
+      this.renderer.setRenderTarget(null);
+      this.finalPass.render(this.renderer, true);
+      this.acumSamples++;
+    }
+  }
+
+  setParams(params) {
+    const keys = Object.keys(params);
+    for (const key of keys) {
+      const value = params[key];
+      if (key === "exposure") {
+        this.finalShader.uniforms.exposure.value = params.exposure;
+      } else {
+        this.material.uniforms[key].value = value;
+        this.lineMaterial.uniforms[key].value = value;
+      }
+    }
+  }
+
+  setSize(w, h) {
+    this.colorFBO.setSize(w, h);
+    this.finalPass.setSize(w, h);
+  }
+
+  reset() {
+    this.acumSamples = 0;
+    this.renderer.setRenderTarget(this.colorFBO);
+    this.renderer.clear();
+  }
+
+  clear() {
+    while (this.scene.children.length) {
+      let m = this.scene.children[0];
+      this.scene.remove(m);
+      m.geometry.dispose();
+      m = null;
+    }
+  }
+}
+
+export { DOFRenderer };
